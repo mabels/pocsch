1a3636f6a7d615960209855b4a03a02b72f0a488
Meno Abels <meno.abels@adviser.com>
2018-01-02 16:07:59 +0100

    * testing

--- aws-cluster/git/code_build_trigger.js
+++ aws-cluster/git/code_build_trigger.js
@@ -1,12 +1,13 @@
 var aws = require('aws-sdk');
 var codecommit = new aws.CodeCommit({ apiVersion: '2015-04-13' });
-var codebuild = new aws.CodeBuild();
+var codebuild = new aws.CodeBuild({apiVersion: '2016-10-06'});
 
 exports.handler = function(event, context) {
     
     //Log the updated references from the event
     var references = event.Records[0].codecommit.references.map(function(reference) {return reference.ref;});
     console.log('References:', references);
+    console.log('CodeCommit:', JSON.stringify(event.Records[0].codecommit));
     
     //Get the repository from the event and show its git clone URL
     var repository = event.Records[0].eventSourceARN.split(":")[5];
@@ -23,11 +24,12 @@ exports.handler = function(event, context) {
             console.log('Clone URL:', data.repositoryMetadata.cloneUrlHttp);
             //context.succeed(data.repositoryMetadata.cloneUrlHttp);
             const build = {
-              'projectName': event['Records'][0]['customData'],
-              'sourceVersion': event['Records'][0]['codecommit']['references'][0]['commit']
+              'projectName': `codebuild-project-${data.repositoryName}`,
+              // 'sourceVersion': event['Records'][0]['codecommit']['references'][0]['commit']
             }
-            console('Starting build for project {0} from commit ID {1}'.format(build['projectName'], build['sourceVersion']))
-            cb.start_build(**build)
+            console.log('Build', build);
+            //console('Starting build for project {0} from commit ID {1}'.format(build['projectName'], build['sourceVersion']))
+            // codebuild.start_build(**build)
         }
     });
 
